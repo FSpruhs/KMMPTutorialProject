3c976f9c306a16cc6f3941b17703e002082b7c24
spruhs <fabian@spruhs.com>
2025-08-29 22:47:00 +0200

    time logic

--- shared/src/commonMain/kotlin/com/spruhs/articles/ArticlesUseCase.kt
+++ shared/src/commonMain/kotlin/com/spruhs/articles/ArticlesUseCase.kt
@@ -1,15 +1,39 @@
 package com.spruhs.articles
 
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.daysUntil
+import kotlin.math.abs
+import kotlin.time.Clock
+import kotlin.time.ExperimentalTime
+import kotlin.time.Instant
+
 class ArticlesUseCase(private val articlesService: ArticlesService) {
 
     suspend fun getArticles(): List<Article> {
         return articlesService.fetchArticles().map { it.toArticle() }
     }
-}
-
-private fun ArticleRaw.toArticle()= Article(
+    private fun ArticleRaw.toArticle() = Article(
         title = title,
         description = desc ?: "Click to find out more",
         imageUrl = imageUrl ?: "https://image.cnbcfm.com/api/v1/image/107326078-1698758530118-gettyimages-1765623456-wall26362_igj6ehhp.jpeg?v=1698758587&w=1920&h=1080",
-        date = date,
+        date = getDaysAgoString(date)
     )
+
+    @OptIn(ExperimentalTime::class)
+    private fun getDaysAgoString(date: String): String {
+        val today = Clock.System.now()
+        val days = today.daysUntil(
+            Instant.parse(date),
+            timeZone = TimeZone.currentSystemDefault()
+        )
+
+        val result = when {
+            abs(days) > 1 -> "$days days ago"
+            abs(days) == 1 -> "Yesterday"
+            else -> "Today"
+        }
+
+        return result
+    }
+}
+
